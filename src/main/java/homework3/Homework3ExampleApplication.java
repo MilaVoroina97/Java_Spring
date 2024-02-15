249943462a1ab506fbc74949adc29145d98b41d2
mila <milavoronina1997@mail.ru>
2024-02-15 22:06:25 +0500

    add entrypoint

---
+++ src/main/java/homework3/Homework3ExampleApplication.java
@@ -0,0 +1,14 @@
+package homework3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework3ExampleApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(Homework3ExampleApplication.class,args);
+
+    }
+}
